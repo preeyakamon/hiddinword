20493b99ce6902b9c358fdc7db616bcd28aaeb68
apimun <apimun.ksk@gmail.com>
2017-05-30 19:16:30 +0700

    add playable.java by apimun.ksk

---
+++ app/src/main/java/apprtc/preeyakamon/hiddinword/playable.java
@@ -0,0 +1,17 @@
+package apprtc.preeyakamon.hiddinword;
+
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+
+/**
+ * Created by apimun on 5/30/17.
+ */
+
+public class playable extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_play);
+    }
+}
